c17d0fdd82c1d5ccf662add0eacfa51d6c42fd68
maxim_nikulin <ddnikulinm@gmail.com>
2022-04-06 10:54:06 +0500

    Initial commit

---
+++ app/src/main/java/tribore/musicplayercrossfaider/MainActivity.kt
@@ -0,0 +1,11 @@
+package tribore.musicplayercrossfaider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
